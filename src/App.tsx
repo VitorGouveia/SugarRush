42d08b578bcb739cb0e1c5911c22bb3072752a95
Rodrigo Andrade <rdrade121@gmail.com>
2022-06-06 21:03:55 -0300

    product mapping
    created product and category mapping, with images

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,29 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import Products from './db.json'
+import {Button} from './Components/Button.style'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      {
+        Products.map( db => {
+          return(
+            <div className="products-box">
+             <h1>{ db.category }</h1>
+              {db.products.map( db => {
+               return(
+                <div>
+                <h3>{db.name}</h3>
+                <p>{db.description}</p>
+                <p>R${db.price}</p>
+                <img src={db.image}></img>
+                </div>
+               )
+             })}
+            </div>
+          )
+        })
+        }
     </div>
   )
 }
