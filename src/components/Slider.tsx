ec6069860999809942a5adfa60b9453a7f5458cc
AtomicFeasT <atomic-feast@hotmail.com>
2022-06-07 18:08:09 -0300

    feat(_header.scss): improved styling

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-
 export function Slider() {
     window.addEventListener('load', function(){
         // @ts-ignore
@@ -9,32 +7,12 @@ export function Slider() {
             slidesToShow: 3,
             draggable: true,
             dots: '.dots',
+            itemWidth: 100,
+            itemHeight: 100,
             arrows: {
                 prev: '.glider-prev',
                 next: '.glider-next'
             },
-            responsive: [
-              {
-                // screens greater than >= 775px
-                breakpoint: 775,
-                settings: {
-                  // Set to `auto` and provide item width to adjust to viewport
-                  slidesToShow: 'auto',
-                  slidesToScroll: 'auto',
-                  itemWidth: 150,
-                  duration: 0.25
-                }
-              },{
-                // screens greater than >= 1024px
-                breakpoint: 1024,
-                settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 1,
-                  itemWidth: 150,
-                  duration: 0.25
-                }
-              }
-            ]
           });   
         })
     
