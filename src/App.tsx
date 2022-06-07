8882afbf79be206bf6dab503c1c96e06c256c0f2
AtomicFeasT <atomic-feast@hotmail.com>
2022-06-07 17:07:13 -0300

    feat(styles & components): created some styling and some components
    added glider.js aswell

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,39 @@
 import './App.css'
 import Products from './db.json'
-import {Button} from './Components/Button.style'
+import './styles/main.scss'
+
+import { Header } from './components/Header'
+import { Slider } from './components/Slider'
 
 function App() {
   return (
-    <div className="App">
-      {
-        Products.map( db => {
-          return(
-            <div className="products-box">
-             <h1>{ db.category }</h1>
+        <div className="App">
+          {/* {
+          Products.map( db => {
+            return(
+              <div className="products-box">
+              <h1>{ db.category }</h1>
               {db.products.map( db => {
-               return(
-                <div>
-                <h3>{db.name}</h3>
-                <p>{db.description}</p>
-                <p>R${db.price}</p>
-                <img src={db.image}></img>
+                return(
+                  <div>
+                  <h3>{db.name}</h3>
+                  <p>{db.description}</p>
+                  <p>R${db.price}</p>
+                  <img src={db.image}></img>
+                  </div>
+                  )
+                })}
                 </div>
-               )
-             })}
-            </div>
-          )
-        })
-        }
-    </div>
-  )
-}
-
-export default App
+                )
+              })
+            } */}
+            <main>
+              <Header/>
+              <Slider/>
+            </main>
+        </div>
+        )
+      }
+      
+      export default App
+      
---
+++ src/components/Slider.tsx
@@ -0,0 +1,65 @@
+import { useEffect } from "react";
+
+export function Slider() {
+    window.addEventListener('load', function(){
+        // @ts-ignore
+        new Glider(document.querySelector('.glider'), {
+            // Mobile-first defaults
+            slidesToScroll: 1,
+            slidesToShow: 2,
+            draggable: true,
+            dots: '.dots',
+            arrows: {
+                prev: '.glider-prev',
+                next: '.glider-next'
+            },
+            responsive: [
+              {
+                // screens greater than >= 775px
+                breakpoint: 775,
+                settings: {
+                  // Set to `auto` and provide item width to adjust to viewport
+                  slidesToShow: 'auto',
+                  slidesToScroll: 'auto',
+                  itemWidth: 150,
+                  duration: 0.25
+                }
+              },{
+                // screens greater than >= 1024px
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+                  itemWidth: 150,
+                  duration: 0.25
+                }
+              }
+            ]
+          });   
+        })
+    
+    return (
+            <div className="glider-container">
+                <div className="glider">
+                    <div className="glider-item"> 
+                        <h2>Pães</h2>
+                    </div>
+                    <div className="glider-item"> 
+                        <h2>Pães</h2>
+                    </div>
+                    <div className="glider-item"> 
+                        <h2>Pães</h2>
+                    </div>
+                    <div className="glider-item"> 
+                        <h2>Pães</h2>
+                    </div>
+                </div>
+            
+                <button aria-label="Previous" className="glider-prev">«</button>
+                <button aria-label="Next" className="glider-next">»</button>
+                <div role="tablist" className="dots"></div>
+            </div>
+
+        
+        )
+    }
