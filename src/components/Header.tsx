bdf577148b6ab952dfb138994171120fc990a922
AtomicFeasT <atomic-feast@hotmail.com>
2022-06-09 23:34:25 -0300

    refactor(header): made some small changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 export function Header() {
     return (
-        <header className="header">
-            <h2>YesCakes</h2>
-           <div className="social-icons">               
-               <svg className="facebook-logo" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z" stroke="#732640" stroke-width="0.7" stroke-linecap="round" stroke-linejoin="round"/><path d="M21 11H19C18.6056 10.9984 18.2147 11.0748 17.85 11.225C17.4853 11.3752 17.1539 11.5961 16.875 11.875C16.5961 12.1539 16.3752 12.4853 16.225 12.85C16.0748 13.2147 15.9984 13.6056 16 14V28" stroke="#732640" stroke-width="0.7" stroke-linecap="round" stroke-linejoin="round"/><path d="M12 18H20" stroke="#732640" stroke-width="0.7" stroke-linecap="round" stroke-linejoin="round"/></svg>
-               <svg className="instagram-logo" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 21C18.7614 21 21 18.7614 21 16C21 13.2386 18.7614 11 16 11C13.2386 11 11 13.2386 11 16C11 18.7614 13.2386 21 16 21Z" stroke="#732640" stroke-width="0.7" stroke-miterlimit="10"/><path d="M21.5 4.5H10.5C7.18629 4.5 4.5 7.18629 4.5 10.5V21.5C4.5 24.8137 7.18629 27.5 10.5 27.5H21.5C24.8137 27.5 27.5 24.8137 27.5 21.5V10.5C27.5 7.18629 24.8137 4.5 21.5 4.5Z" stroke="#732640" stroke-width="0.7" stroke-linecap="round" stroke-linejoin="round"/><path d="M23.95 9.5C23.95 10.3008 23.3008 10.95 22.5 10.95C21.6992 10.95 21.05 10.3008 21.05 9.5C21.05 8.69919 21.6992 8.05 22.5 8.05C23.3008 8.05 23.95 8.69919 23.95 9.5Z" fill="#732640" stroke="#732640" stroke-width="0.1"/></svg>
-           </div>
+        <header className="header">            
+        <h2>YesCakes</h2>
+        <div className="social-icons">               
+            <svg className="facebook-logo" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z" stroke="#732640" stroke-width="0.7" stroke-linecap="round" stroke-linejoin="round"/><path d="M21 11H19C18.6056 10.9984 18.2147 11.0748 17.85 11.225C17.4853 11.3752 17.1539 11.5961 16.875 11.875C16.5961 12.1539 16.3752 12.4853 16.225 12.85C16.0748 13.2147 15.9984 13.6056 16 14V28" stroke="#732640" stroke-width="0.7" stroke-linecap="round" stroke-linejoin="round"/><path d="M12 18H20" stroke="#732640" stroke-width="0.7" stroke-linecap="round" stroke-linejoin="round"/></svg>
+            <svg className="instagram-logo" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 21C18.7614 21 21 18.7614 21 16C21 13.2386 18.7614 11 16 11C13.2386 11 11 13.2386 11 16C11 18.7614 13.2386 21 16 21Z" stroke="#732640" stroke-width="0.7" stroke-miterlimit="10"/><path d="M21.5 4.5H10.5C7.18629 4.5 4.5 7.18629 4.5 10.5V21.5C4.5 24.8137 7.18629 27.5 10.5 27.5H21.5C24.8137 27.5 27.5 24.8137 27.5 21.5V10.5C27.5 7.18629 24.8137 4.5 21.5 4.5Z" stroke="#732640" stroke-width="0.7" stroke-linecap="round" stroke-linejoin="round"/><path d="M23.95 9.5C23.95 10.3008 23.3008 10.95 22.5 10.95C21.6992 10.95 21.05 10.3008 21.05 9.5C21.05 8.69919 21.6992 8.05 22.5 8.05C23.3008 8.05 23.95 8.69919 23.95 9.5Z" fill="#732640" stroke="#732640" stroke-width="0.1"/></svg>
+        </div>
         </header>
     )
 }
