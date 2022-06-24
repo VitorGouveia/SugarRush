41f8c877d01a481327a320c9fad0e282c6c9882d
AtomicFeasT <atomic-feast@hotmail.com>
2022-06-24 18:06:15 -0300

    feat(src): removed images, added fixed heights and more
    changed footer styling too

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,38 +1,35 @@
-import { useState } from 'react';
 import Products from '../db.json';
 
+export function Category({ parentToChild }: any) {
+
+  const Category = Products.filter((db: { category: string | null; }) => {
 
 
+    return db.category === parentToChild
 
-export function Category({parentToChild}: any){
-  
-  const Category = Products.filter((db: { category: string | null ;}) => {
-    
-
-      return db.category === parentToChild
-    
   })
   return (
     <section className="product-section">
-      { 
-        Category.map( db => {
-          return(
+      {
+        Category.map(db => {
+          return (
             <>
-              {db.products.map( product => {
-                return(
+              {db.products.map(product => {
+                return (
                   <div className="card-container">
-                  <div className="card-content">
-                       <h1 className="card-title">{product.name}</h1>
-                       <p className="card-description">{product.description}</p>
-                       <h2 className="card-price">R$ {product.price}</h2>
+                    <div className="card-content">
+                      <h1 className="card-title">{product.name}</h1>
+                      <p className="card-description">{product.description}</p>
+                      <h2 className="card-price">R$ {product.price}</h2>
+                    </div>
+                    {/* <img src={product.image} alt={product.name} className="card-image" /> */}
                   </div>
-                  <img src={product.image} alt={product.name} className="card-image" />
-              </div>
-                  )
-                })}
+                )
+              })}
             </>
-          )})
-        }
+          )
+        })
+      }
     </section>
-    )
-  }
+  )
+}
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,75 +1,71 @@
 import Products from '../db.json'
-import {FormEventHandler, useState} from 'react';
+import { FormEventHandler, useState } from 'react';
+
+export function Slider({ childToParent }: any) {
+
+  window.addEventListener('load', function() {
+    // @ts-ignore
+    new Glider(document.querySelector('.glider'), {
+      // Mobile-first defaults
+      slidesToScroll: 2,
+      slidesToShow: 2,
+      draggable: true,
+      dots: '.dots',
+      arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+      },
+      responsive: [
+        {
+          // screens greater than >= 775px
+          breakpoint: 445,
+          settings: {
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: 'auto',
+            slidesToScroll: 'auto',
+            itemWidth: 150,
+            duration: 0.25
+          }
+        }, {
+          // screens greater than >= 1024px
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 'auto',
+            slidesToScroll: 'auto',
+            itemWidth: 160,
+            duration: 0.25
+          }
+        }
+      ]
+    });
+  })
+
+  return (
+    <div className="glider-container">
+      <div className="glider">
+        {Products.map(db => {
 
 
+          const changeFilterData = (filter: string) => () => childToParent(filter)
+          return (
 
-export function Slider({childToParent}: any) {
-
-    
-
-    window.addEventListener('load', function(){
-        // @ts-ignore
-        new Glider(document.querySelector('.glider'), {
-            // Mobile-first defaults
-            slidesToScroll: 2,
-            slidesToShow: 2,
-            draggable: true,
-            dots: '.dots',
-            arrows: {
-                prev: '.glider-prev',
-                next: '.glider-next'
-            },
-            responsive: [
-              {
-                // screens greater than >= 775px
-                breakpoint: 445,
-                settings: {
-                  // Set to `auto` and provide item width to adjust to viewport
-                  slidesToShow: 'auto',
-                  slidesToScroll: 'auto',
-                  itemWidth: 150,
-                  duration: 0.25
-                }
-              },{
-                // screens greater than >= 1024px
-                breakpoint: 1024,
-                settings: {
-                  slidesToShow: 'auto',
-                  slidesToScroll: 'auto',
-                  itemWidth: 160,
-                  duration: 0.25
-                }
-              }
-            ]
-          });   
-        })
-    
-    return (
-            <div className="glider-container">
-              <div className="glider">
-              {Products.map( db =>{
-                
-                
-                const changeFilterData = (filter: string) => () => childToParent(filter)
-                return(
-              
-                    <div onClick={changeFilterData(db.category)} className="glider-item"> 
-                        <img className="category-image" src={db.img} alt="" />
-                        <h2 className="category-name">{db.category}</h2>
-                    </div>
-                    
-            )
-              } )}
-
-            </div>
-                <div role="tablist" className="dots"></div>
-
-                <div className="glider-movement-container">
-                  <button aria-label="Previous" className="glider-prev">«</button>
-                  <button aria-label="Next" className="glider-next">»</button>
-                </div>
+            <div onClick={changeFilterData(db.category)} className="glider-item">
+              <img className="category-image" src={db.img} alt="" />
+              <h2 className="category-name">{db.category}</h2>
             </div>
 
+          )
+        })}
 
-        )
-    }
+      </div>
+      <div role="tablist" className="dots"></div>
+
+      <div className="glider-movement-container">
+        <button aria-label="Previous" className="glider-prev">«</button>
+        <button aria-label="Next" className="glider-next">»</button>
+      </div>
+    </div>
+
+
+  )
+}
